5be23ae16c3d94d70b15c9a1dfb29ec667154aed
sierrajulietromeo <steven.john.rich@gmail.com>
2017-11-12 17:22:26 +0000

    Sunday PM improvements. One CSV file, reduced function redundancy.

--- test_booking.py
+++ test_booking.py
@@ -11,96 +11,104 @@ def main():
 
 
     mainMenuSelection = int(input("Enter selection: "))
-
+    os.system("cls")  #Only works on Windows machines
     if(mainMenuSelection == 1):
-
-        os.system("cls")  #Only works on Windows machines
-        viewTueRoomBookings()
-    if(mainMenuSelection == 2):
-        os.system("cls")  #Only works on Windows machines
-        viewWedRoomBookings()
+        selectedDay = "Tuesday"
+        viewRoomBookings(selectedDay)
+    elif(mainMenuSelection == 2):
+        selectedDay = "Wednesday"
+        viewRoomBookings(selectedDay)
     else:
         main()
 
-def viewTueRoomBookings():
-    print("VIEW TUESDAY ROOM BOOKINGS")
+
+    viewRoomBookings(selectedDay)
+
+
+def viewRoomBookings(selectedDay):
+    print("VIEW ROOM BOOKINGS FOR:",selectedDay)
     print("=================================")
     print("Please select from the following rooms")
     print("1: SPUTNIK")
     print("2: ENDEAVOUR")
     print("3: VOYAGER")
 
-    viewTueSelection = int(input("Enter selection: "))
-
-    if(viewTueSelection == 1):
-        print("1")
-        os.system("cls")  #Only works on Windows machines
-        displayRoomBookings("sputnik_tues.csv")
-    elif(viewTueSelection == 2):
-        print("2")
-    elif(viewTueSelection == 3):
+    viewSelection = int(input("Enter selection: "))
+    os.system("cls")  #Only works on Windows machines
+    if(viewSelection == 1):
+        roomName = "Sputnik"
+        displayRoomBookings(roomName,selectedDay)
+    elif(viewSelection == 2):
+        displayRoomBookings(selectedDay)
+    elif(viewSelection == 3):
         print("3")
     else:
         main()
 
-def viewWedRoomBookings():
-    print("VIEW WEDNESDAY ROOM BOOKINGS")
-    print("=================================")
-    print("Please select from the following rooms:")
-    print("1: SPUTNIK")
-    print("2: ENDEAVOUR")
-    print("3: VOYAGER")
-    print("4: Back to main menu.")
-
-    viewWedSelection = int(input("Enter selection: "))
-
-    if(viewWedSelection == 1):
-        print("1")
-        displayRoomBookings("sputnik_tues.csv")
-    elif(viewWedSelection == 2):
-        print("2")
-    elif(viewWedSelection == 3):
-        print("3")
+def displayRoomBookings(roomName,selectedDay):
+    df = pd.read_csv("room_bookings.csv")
+    #df.set_index("Period")
+    if(selectedDay == "Tuesday"):
+        firstRow = 0
+        lastRow = 7
+    elif(selectedDay == "Wednesday"):
+        firstRow = 7
+        lastRow = 15
     else:
         main()
 
-def displayRoomBookings(roomName):
-    df = pd.read_csv(roomName)
-    print(roomName +" BOOKINGS")
+
+    print(selectedDay +" Bookings")
 
     #print (df(index=False))
-    print(df.head(7) ) #prints periods for the day (is df head the best to use?)
-    print("Enter the period number you wish to book, or enter N to go back.")
+    print(df.set_index("Period")[firstRow:lastRow] ) #prints periods for the day (is df head the best to use?)
+    #print(df.set_index("Period").head(7) ) #prints periods for the day (is df head the best to use?)
+    print("Enter the period number you wish to book, or enter any other character to go back.")
 
     try:
         periodNumber = int(input("?"))
+        #os.system("cls")  #Only works on Windows machines
     except ValueError:
         os.system("cls")  #Only works on Windows machines
         main()
 
     if(periodNumber == 1 or 2 or 3 or 4 or 5 or 6 or 7):
-        bookRoom(roomName, periodNumber)
+        bookRoom(roomName, periodNumber, selectedDay)
     else:
         os.system("cls")  #Only works on Windows machines
         main()
 
 
-def bookRoom(roomName, periodNumber):
-    df = pd.read_csv(roomName)
+def bookRoom(roomName, periodNumber, selectedDay):
+    df = pd.read_csv("room_bookings.csv")
+
+    if(roomName == "Sputnik" and selectedDay == "Wednesday"):
+        periodNumber += 7
+    else:
+        periodNumber
+
+
     periodNumber -= 1 #to get the right place in the array because counting starts at 0
 
     if (df.at[periodNumber, "Name"] != "FREE"):
         os.system("cls")  #Only works on Windows machines
+        print ('\a')
         print("Sorry, this room is already booked for that period by:", (df.at[periodNumber, "Name"])  )
         print("                                  ")
-        displayRoomBookings(roomName)
+        displayRoomBookings(roomName, selectedDay)
     else:
         name = input("Enter name to book room under: ")
         df.at[periodNumber, "Name"] = name
-        df.to_csv("sputnik_tues.csv", index=False)
+        df.to_csv("room_bookings.csv", index=False)
+
+        if(roomName == "Sputnik" and selectedDay == "Wednesday"):
+            periodNumber -= 7
+        else:
+            periodNumber
+
         periodNumber += 1 #to get the right place in the array because counting starts at 0
-        os.system("cls")  #Only works on Windows machines
-        print("Room is now booked for you at period: ", periodNumber)
+        #os.system("cls")  #Only works on Windows machines
+        print("Room is now booked for you at period:", periodNumber, "on", selectedDay)
         print("                           ")
 
 
@@ -114,7 +122,7 @@ def bookRoom(roomName, periodNumber):
 
 def editRoomBooking():
     #df.at[1, "Name"] = "John"
-    df.to_csv("sputnik_tues.csv", index=False)
+    df.to_csv("room_bookings.csv", index=False)
 
 
 #Sequential Code
